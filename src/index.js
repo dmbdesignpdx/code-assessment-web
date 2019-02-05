54fb741a9bfa3951ffe93b3954ff681472714532
dmbdesignpdx <daniel@danielblake.design>
2019-02-04 17:26:38 -0800

    sets up global styles and breakpoints

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import { createLogger } from 'redux-logger'
 import thunk from 'redux-thunk'
+
 import reducer from './reducers'
 import { getAllProducts } from './actions'
 import App from './containers/App'
+import "./styles/master.css"
+
 
 const middleware = [ thunk ];
 if (process.env.NODE_ENV !== 'production') {
